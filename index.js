d68144bd7f090ad0d5b6808fa30045968151ff04
Fábio <videiraft@gmail.com>
2019-08-15 19:48:06 +0200

    implement it

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // Determine if a number can be considered narcissitic
 
 function doILookBigInThis(value) {
-
+  const str = value + ''
+  const strLen = str.length
+  let total = 0
+  for (let i = 0; i < str.length; i++) {
+    total += Math.pow(str[i], strLen)
+  }
+  if (total === value) return true
+  return false
 }
 
 
