17e4c2af7c6f494631c5e68905dc453f581fd694
Florian Jost <florian.jost89@gmail.com>
2019-08-15 19:50:33 +0200

    refactor

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function doILookBigInThis(value) {
   const number = String(value).split('').map(el => Number(el));
   const base = Number(number.length);
 
-  const newArr = number.map(el => Math.pow(el, base)).reduce((a,b) => a+b);
-  if (value === newArr) return true;
+  const sum = number.map(el => Math.pow(el, base)).reduce((a,b) => a+b);
+  if (value === sum) return true;
   return false;
 }
 
