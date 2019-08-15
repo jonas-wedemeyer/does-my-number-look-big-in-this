39f4daaf98a004ce5a6eef929ce9d28cd38df79d
Florian Jost <florian.jost89@gmail.com>
2019-08-15 19:48:15 +0200

    works -> refactoring needed

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Determine if a number can be considered narcissitic
 
 function doILookBigInThis(value) {
+  const number = String(value).split('').map(el => Number(el));
+  const base = Number(number.length);
 
+  const newArr = number.map(el => Math.pow(el, base)).reduce((a,b) => a+b);
+  if (value === newArr) return true;
+  return false;
 }
 
 
